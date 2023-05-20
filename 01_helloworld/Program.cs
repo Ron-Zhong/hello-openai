39dd879205432ae0074a163486e60c6b8dde96ed
Ron Zhong <ron.zhong@hotmail.com>
2023-05-20 20:21:21 +0800

    move to ./01_helloworld folder

---
+++ 01_helloworld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+IConfiguration Configuration = builder.Build();
+
+var AZURE_OPENAI_ENABLED = true;
+var AZURE_OPENAI_ENDPOINT = "https://poc-openai-mims.openai.azure.com/";
+var AZURE_OPENAI_API_KEY = Configuration["azure-openai-api-key"] ?? string.Empty;
+var OPENAI_API_KEY = Configuration["openai-api-key"] ?? string.Empty;
+
+OpenAIClient client = AZURE_OPENAI_ENABLED
+    ? new OpenAIClient(
+        new Uri(AZURE_OPENAI_ENDPOINT),
+        new AzureKeyCredential(AZURE_OPENAI_API_KEY))
+    : new OpenAIClient(OPENAI_API_KEY);
+
+Response<Completions> response = await client.GetCompletionsAsync(
+    "text-davinci-003", 
+    "Hello, world!");
+
+foreach (Choice choice in response.Value.Choices)
+{
+    Console.WriteLine(choice.Text);
+}
