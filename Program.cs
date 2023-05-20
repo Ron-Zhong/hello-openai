ed2a068394a2b3eeb9b59e048f630c9b64e73a55
Ron Zhong <ron.zhong@hotmail.com>
2023-05-20 20:08:29 +0800

    add OpenAI SDK

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+IConfiguration Configuration = builder.Build();
+
+var USE_AZURE_OPENAI = false;
+var OPENAI_API_KEY = Configuration["openai-api-key"];
+
+OpenAIClient client = USE_AZURE_OPENAI
+    ? new OpenAIClient(
+        new Uri("https://your-azure-openai-resource.com/"),
+        new AzureKeyCredential("your-azure-openai-resource-api-key"))
+    : new OpenAIClient(OPENAI_API_KEY);
+
+Response<Completions> response = await client.GetCompletionsAsync(
+    "text-davinci-003", // assumes a matching model deployment or model name
+    "Hello, world!");
+
+foreach (Choice choice in response.Value.Choices)
+{
+    Console.WriteLine(choice.Text);
+}
