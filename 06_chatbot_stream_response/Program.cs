063b0c447cc7fc60ef2e0d8608815ed0941562b5
Ron Zhong <ron.zhong@hotmail.com>
2023-05-20 23:47:43 +0800

    add OpenAI SDK sample code

---
+++ 06_chatbot_stream_response/Program.cs
@@ -0,0 +1,54 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.Configuration;
+
+// Initialize configuration
+var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+IConfiguration Configuration = builder.Build();
+
+var IS_AZURE_OPENAI = true;
+var AZURE_OPENAI_ENDPOINT = "https://poc-openai-mims.openai.azure.com/";
+var AZURE_OPENAI_API_KEY = Configuration["azure-openai-api-key"] ?? string.Empty;
+var OPENAI_API_KEY = Configuration["openai-api-key"] ?? string.Empty;
+
+OpenAIClient client = IS_AZURE_OPENAI
+    ? new OpenAIClient(
+        new Uri(AZURE_OPENAI_ENDPOINT),
+        new AzureKeyCredential(AZURE_OPENAI_API_KEY))
+    : new OpenAIClient(OPENAI_API_KEY);
+
+
+// Sample Code: Stream Chat Messages with non-Azure OpenAI
+while (true)
+{
+    Console.WriteLine("You:");
+    var input = Console.ReadLine();
+
+    var chatCompletionsOptions = new ChatCompletionsOptions()
+    {
+        Messages =
+        {
+            new ChatMessage(ChatRole.User, input),
+        }
+    };
+
+    Console.WriteLine();
+    Console.WriteLine("OpenAI:");
+
+    Response<StreamingChatCompletions> response = await client.GetChatCompletionsStreamingAsync(
+        deploymentOrModelName: "gpt-35-turbo",
+        chatCompletionsOptions);
+    using StreamingChatCompletions streamingChatCompletions = response.Value;
+
+    await foreach (StreamingChatChoice choice in streamingChatCompletions.GetChoicesStreaming())
+    {
+        await foreach (ChatMessage message in choice.GetMessageStreaming())
+        {
+            Console.Write(message.Content);
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
